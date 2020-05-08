bdf4583aae5f6adc6792fe9cac395df40ba1b6a1
James Riall <jamesrariall@gmail.com>
2020-05-08 10:40:39 +0100

    Add ngrx modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+
+import {environment} from '../environments/environment';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
@@ -11,6 +17,13 @@ import {AppComponent} from './app.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     BrowserModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      logOnly: environment.production,
+      maxAge: 25,
+    }),
+    StoreRouterConnectingModule.forRoot(),
+    EffectsModule.forRoot([]),
   ],
   bootstrap: [AppComponent],
 })

6d2f05ba521bcdc42c2c47dae32ab29ccbd59553
James Riall <jamesrariall@gmail.com>
2020-05-10 16:36:17 +0100

    Add remaining API methods to interact with clients

--- backend/main.py
+++ backend/main.py
@@ -38,5 +38,36 @@ def clients():
         raise Exception('Method not supported')
 
 
+@app.route('/api/clients/<id>', methods=['DELETE', 'GET', 'PATCH'])
+@requires_auth_token
+def client(id):
+    if request.method == 'GET':
+        client = client_ref.document(id).get().to_dict()
+        if client:
+            return jsonify(client), 200
+        else:
+            raise Exception('Client not found')
+    if request.method == 'DELETE':
+        client = client_ref.document(id).get().to_dict()
+        if client:
+            client_ref.document(id).delete()
+            return jsonify({'success': True}), 200
+        else:
+            raise Exception('Client not found')
+    if request.method == 'PATCH':
+        client = client_ref.document(id).get().to_dict()
+        if client:
+            try:
+                name = request.json['name']
+                client_ref.document(id).update(Client(name=name).to_dict())
+                return jsonify({'success': True}), 200
+            except:
+                raise Exception('Client object not formatted correctly')
+        else:
+            raise Exception('Client not found')
+    else:
+        raise Exception('Method not supported')
+
+
 if __name__ == "__main__":
     app.run()
